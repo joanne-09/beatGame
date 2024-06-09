a96ff5ee5dca9f69ce177f22295a49326738d5cd
Windmill10 <eason200506@gmail.com>
2024-06-09 17:30:38 +1000

    more modifications

--- main.cpp
+++ main.cpp
@@ -3,26 +3,26 @@
 // You can register your scenes here, and start the game.
 #include "Engine/GameEngine.hpp"
 #include "Engine/LOG.hpp"
-#include "Scene/LoseScene.hpp"
+#include "Scene/MainMenu.hpp"
+#include "Scene/MusicSelection.hpp"
 #include "Scene/PlayScene.hpp"
-#include "Scene/StageSelectScene.hpp"
-#include "Scene/WinScene.hpp"
-#include "Scene/StartScene.h"
-#include "Scene/SettingsScene.hpp"
-#include "Scene/ScoreBoardScene.hpp"
+#include "Scene/Scoreboard.hpp"
+#include "Scene/Settings.hpp"
+
 
 int main(int argc, char **argv) {
 	Engine::LOG::SetConfig(true);
 	Engine::GameEngine& game = Engine::GameEngine::GetInstance();
 
     // TODO: [HACKATHON-1-SCENE] (3/4): Register Scenes here
-    game.AddNewScene("start", new StartScene());
+    /*game.AddNewScene("start", new StartScene());
 	game.AddNewScene("settings", new SettingsScene());
 	game.AddNewScene("play", new PlayScene());
 	game.AddNewScene("lose", new LoseScene());
 	game.AddNewScene("win", new WinScene());
     game.AddNewScene("stage-select", new StageSelectScene());
-    game.AddNewScene("scoreboard", new ScoreBoardScene());
+    game.AddNewScene("scoreboard", new ScoreBoardScene());*/
+
 
     // TODO: [HACKATHON-1-SCENE] (4/4): Change the start scene
 //	game.Start("stage-select", 60, 1600, 832);
