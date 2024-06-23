ba12a6e61e60ff8cc29ea6b46239aa046c0f3ef3
Windmill10 <eason200506@gmail.com>
2024-06-23 14:36:39 +0800

    updated beatmaps

--- pythonfiles/beatsgen.py
+++ pythonfiles/beatsgen.py
@@ -1,27 +1,34 @@
 import random
 from random import randint
-song_name = input("Enter song name: ")
-bpm = int(input("Enter bmp: "))
-seconds = int(input("Enter seconds: "))
-buffer_lines = 0;
-total_lines = 2*bpm*(seconds/60)
+info = [["night", "136", "162"], ["ASMRZ", "130", "149"], ["blingbling", "157", "172"], ["magnetic", "131", "138"],
+        ["night_dancer", "117", "210"], ["racing_night", "130", "261"], ["unforgiven", "104", "189"],
+        ["waiting_for_love", "128", "230"]]
+for song in range(len(info)):
+    '''song_name = input("Enter song name: ")
+    bpm = int(input("Enter bpm: "))
+    seconds = int(input("Enter seconds: "))'''
+    song_name = info[song][0]
+    bpm = int(info[song][1])
+    seconds = int(info[song][2])
+    buffer_lines = 0;
+    total_lines = 2*bpm*(seconds/60)
 
-def generate_beats() -> str:
-    beats = ""
-    one_pos = random.randint(0, 3)
-    for i in range(0, 4):
-        if i == one_pos:
-            beats += "1"
-        else:
-            beats += "0"
-    return beats + "\n"
+    def generate_beats() -> str:
+        beats = ""
+        one_pos = random.randint(0, 3)
+        for i in range(0, 4):
+            if i == one_pos:
+                beats += "1"
+            else:
+                beats += "0"
+        return beats + "\n"
 
-with open(song_name+".txt", "w") as f:
-    for i in range(int(total_lines)):
-        if(i < buffer_lines):
-            f.write("0000\n")
-            continue;
-        if(i % 2 == 0):
-            f.write(generate_beats())
-        else:
-            f.write("0000\n")
+    with open(song_name+".txt", "w") as f:
+        for i in range(int(total_lines)):
+            if(i < buffer_lines):
+                f.write("0000\n")
+                continue;
+            if(i % 2 == 0):
+                f.write(generate_beats())
+            else:
+                f.write("0000\n")
